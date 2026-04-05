1f1cda655e1c01b29c3240e9e05d61333445b8c2
PaxiMoos <rubenradermacher70@gmail.com>
2026-04-06 01:02:43 +0200

    Klasse LagerAuslastung erstellt.

---
+++ Logistik/Lagers/LagerAuslastung.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Logistik.Lagers;
+
+public class LagerAuslastung
+{
+    public int LagerKapazität { get; set; }
+    public int Bestand { get; set; }
+
+    public void LagerAuslastungAnzeigen()
+    {
+        System.Console.WriteLine($"Die Lagerauslastung liegt bei {LagerKapazität} %.");
+    }
+
+    public void LagerFastVoll()
+    {
+        if (Bestand > 80)
+        {
+            System.Console.WriteLine("Das Lager ist fast voll, bittet verkauft Ware.");
+        }
+    }
+
+    public void LagerFastLeer()
+    {
+        if (Bestand < 20)
+        {
+            System.Console.WriteLine("Das Lager ist fast leer, bitt kauft mehr Ware ein.");
+        }
+    }
+}
